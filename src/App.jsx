b8f29a5b2e50cfd6b1edb02ae1cee452c16f1c4a
JamshidPardayev <jamshidnt2024@gmail.com>
2025-04-14 18:49:49 +0500

    hatolik tugirlandi

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,7 @@ function App() {
           )}
         </div>
       </div>
-      {/* routeslar */}
+
       <Routes>
         <Route path="/" element={<LoginForm />} />
         <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
