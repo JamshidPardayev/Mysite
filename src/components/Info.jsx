4410e840ccd09f35416dc19f46f99c9bc739b5d4
JamshidPardayev <jamshidnt2024@gmail.com>
2025-04-14 19:48:48 +0500

    hatolar tugirlandi

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -4,7 +4,7 @@ const Info = () => {
   return (
     <div className="p-6">
         <h1 className="text-2xl font-bold text-green-700">Welcome to Info!</h1>
-        <p className="text-xl mt-2 font-bold text-green-700">I'm Pardayev Jamshid. I 23 years old. I was born Uzbekistan. I'm Web Developer 😊</p>
+        <p className="text-xl mt-2 font-bold text-green-700">I'm Pardayev Jamshid. I am 23 years old. I was born Uzbekistan. I'm Web Developer 😊</p>
 
       </div>
   )
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -31,8 +31,8 @@ const LoginForm = () => {
   return (
     <form onSubmit={handleSubmit} className=" flex flex-col gap-y-2 p-4 rounded">
       <h2 className="text-xl font-bold">Login</h2>
-      <input className='border outline-none h-[40px] px-2 rounded-[8px] border-gray-300' type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
-      <input className='border outline-none h-[40px] px-2 rounded-[8px] border-gray-300' type="text" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <input className='border outline-none h-[40px] px-2 rounded-[8px] border-gray-300' type="email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
+      <input className='border outline-none h-[40px] px-2 rounded-[8px] border-gray-300' type="text" value={password} onChange={(e) => setPassword(e.target.value)} required/>
       <button type="submit" className="mt-4 bg-blue-600 text-white px-4 py-2 rounded-[8px] hover:bg-blue-800 duration-300">
         Login
       </button>
